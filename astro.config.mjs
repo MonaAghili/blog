bac2cdf40923f1557ffd1ae0704672ed390f1d60
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2025-03-30 18:13:13 +0330

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-const isDev = process.env.NODE_ENV === 'development';
-
 export default defineConfig({
-	site: 'https://monaaghili.github.io/',
-	base: isDev ? '/' : '/blog/',
+	site: 'https://monaaghili.github.io/blog',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
