e2a25121ed4f497ff65ecac756305f32ee401458
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2025-03-30 17:32:22 +0330

    init

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'CodexLogica';
+export const SITE_DESCRIPTION = 'Exploring software engineering, one byte at a time';
---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,6 @@
+---
+title: 'Hello World!'
+description: ''
+pubDate: 'March 30 2025'
+---
+
